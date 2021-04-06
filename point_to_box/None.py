0e0fb652d0ef905f4af341c84b92ab30cc27910a
Constantin Baumgartner <constantin.baumgartner@gmail.com>
2021-04-05 21:47:43 -0400

    fastai dev and prompt point refactor

--- point_to_box/None.py
+++ point_to_box/None.py
@@ -107,3 +107,39 @@ import point_to_box.model as ptb_model
 import point_to_box.utils as utils
 import point_to_box.data as ptb_data
 import point_to_box.model as ptb_model
+
+# Cell
+#export
+import point_to_box.utils as utils
+import point_to_box.data as ptb_data
+import point_to_box.model as ptb_model
+
+# Cell
+#export
+import point_to_box.utils as utils
+import point_to_box.data as ptb_data
+import point_to_box.model as ptb_model
+
+# Cell
+#export
+import point_to_box.utils as utils
+import point_to_box.data as ptb_data
+import point_to_box.model as ptb_model
+
+# Cell
+#export
+import point_to_box.utils as utils
+import point_to_box.data as ptb_data
+import point_to_box.model as ptb_model
+
+# Cell
+#export
+import point_to_box.utils as utils
+import point_to_box.data as ptb_data
+import point_to_box.model as ptb_model
+
+# Cell
+#export
+import point_to_box.utils as utils
+import point_to_box.data as ptb_data
+import point_to_box.model as ptb_model
