ebd437aad381373980ba5f07a67c00b7adf47238
Constantin Baumgartner <constantin.baumgartner@gmail.com>
2021-04-14 10:12:53 -0400

    updates

--- README.md
+++ README.md
@@ -143,3 +143,7 @@ effloc = model.EfficientLoc(version='efficientnet-b0')
 ```
 
 ### Export to onnx
+
+```python
+effloc._export('effloc_coco2017val_v2.onnx', dummy_input = torch.randn(1, 4, 224, 224))
+```
--- point_to_box/None.py
+++ point_to_box/None.py
@@ -641,3 +641,9 @@ import point_to_box.model as ptb_model
 import point_to_box.utils as utils
 import point_to_box.data as ptb_data
 import point_to_box.model as ptb_model
+
+# Cell
+#export
+import point_to_box.utils as utils
+import point_to_box.data as ptb_data
+import point_to_box.model as ptb_model
